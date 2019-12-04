1aef1b670ace2ce2266bdb78df2f96792b00a360
farizkiyazid <25681385+farizkiyazid@users.noreply.github.com>
2019-12-04 13:26:32 +0700

    mantep

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,5 @@
-from app import app
+from app import app, controller
 from flask import render_template, request
-import rdflib
-import re
-
-g=rdflib.Graph()
-g.parse('Movie-RDF.ttl', format="ttl")
 
 @app.route('/')
 @app.route('/index')
@@ -15,32 +10,19 @@ def index():
 @app.route('/search', methods=['POST'])
 def handle_data():
     searchKey = request.form['searchKey']
+    out = '''
+<html>
+    <head>
+        <title>Search Page - Sparkuy</title>
+        '''+controller.style+'''
+    </head>
+    <body>
+        ''' + controller.searchLocal(searchKey) + '''
+    </body>
+</html>'''
+    return out
 
-    limit = 25
-    counter = 0
-    out = ""
-
-    for s,p,o in g:
-        if re.search(searchKey, str(o), re.IGNORECASE):
-            counter+=1
-            title = g.value(s, rdflib.term.URIRef(u'http://localhost:3333/hasTitle'), None)
-            actor = g.value(s, rdflib.term.URIRef(u'http://localhost:3333/hasActor'), None)
-            aspectRatio = g.value(s, rdflib.term.URIRef(u'http://localhost:3333/hasAspectRatio'), None)
-            budget = g.value(s, rdflib.term.URIRef(u'http://localhost:3333/hasBudget'), None)
-            duration = g.value(s, rdflib.term.URIRef(u'http://localhost:3333/hasDuration'), None)
-            genres = g.value(s, rdflib.term.URIRef(u'http://localhost:3333/hasGenres'), None)
-            gross = g.value(s, rdflib.term.URIRef(u'http://localhost:3333/hasGross'), None)
-            imdbLink = g.value(s, rdflib.term.URIRef(u'http://localhost:3333/hasIMDBLink'), None)
-            imdbScore = g.value(s, rdflib.term.URIRef(u'http://localhost:3333/hasIMDBScore'), None)
-            lang = g.value(s, rdflib.term.URIRef(u'http://localhost:3333/hasLanguage'), None)
-            origin = g.value(s, rdflib.term.URIRef(u'http://localhost:3333/hasOrigin'), None)
-            plot = g.value(s, rdflib.term.URIRef(u'http://localhost:3333/hasPlot'), None)
-            Rating = g.value(s, rdflib.term.URIRef(u'http://localhost:3333/hasRating'), None)
-            year = g.value(s, rdflib.term.URIRef(u'http://localhost:3333/madeInYear'), None)
-            votedBy = g.value(s, rdflib.term.URIRef(u'http://localhost:3333/votedBy'), None)
-
-            out += "Movie Id : " + s + "; Movie Actor : "  + actor + "; Movie Title : " + title + "<br>"
-        if counter >= limit:
-            break
-
-    return "test"
+@app.route('/detail', methods=['POST'])
+def handle_detail():
+    Id = request.form['id']
+    return controller.getAttrById(Id)
